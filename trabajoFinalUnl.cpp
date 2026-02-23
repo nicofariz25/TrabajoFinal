69baf1a36d2e0485b358920b1e0896b7483881b5
nicofariz25 <nicofariz76@gmail.com>
2026-02-23 14:05:48 -0300

    agregue el metodo dibujarBordes en la clase juego

--- trabajoFinalUnl.cpp
+++ trabajoFinalUnl.cpp
@@ -32,7 +32,7 @@ void Nave::borrar()
 }
 void Nave::moverIzquierda()
 {
-	if(dx>=2)
+	if(dx>=3)
 	{
 		dx -= 1;
 	}
@@ -40,7 +40,7 @@ void Nave::moverIzquierda()
 }
 void Nave::moverDerecha()
 {
-	if(dx<=30)
+	if(dx<=73)
 	{
 		dx +=1;
 	}
@@ -49,13 +49,24 @@ class Juego
 {
 private:
 	Nave nave1;
+	int bordeX;
+	int bordeX2;
+	int bordeY;
+	int bordeY2;
 public:
 	Juego();
 	void leerTeclasYdibujar();
+	void dibujarBordes();
 	void gameLoop();
 };
 
-Juego::Juego(){}
+Juego::Juego()
+{
+	bordeX = 1;
+	bordeX2 = 1;
+	bordeY = 1;
+	bordeY2 = 1;
+}
 void Juego::leerTeclasYdibujar()
 {
     if(kbhit())
@@ -74,9 +85,38 @@ void Juego::leerTeclasYdibujar()
 		nave1.dibujar();
 	}	
 }
+void Juego::dibujarBordes()
+{
+	if(bordeX<75)
+	{
+		gotoxy(bordeX,1);
+		cout<<".";
+		bordeX++;
+	}
+	
+	if(bordeX2<75)
+	{
+		gotoxy(bordeX2,30);
+		cout<<".";
+		bordeX2++;
+	}
+	if(bordeY<30)
+	{
+		gotoxy(1,bordeY);
+		cout<<".";
+		bordeY++;
+	}
+	if(bordeY2<30)
+	{
+		gotoxy(75,bordeY2);
+		cout<<".";
+		bordeY2++;
+	}
+}
 void Juego::gameLoop()
 {
 	leerTeclasYdibujar();
+	dibujarBordes();
 	
 }
 int main(int argc, char *argv[]) {
